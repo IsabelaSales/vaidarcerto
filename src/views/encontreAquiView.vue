a2286a3a92c42a23e42912276d59588eff7b10d0
IsabelaSales <isa.sales10@hotmail.com>
2022-03-25 15:38:14 -0300

    Editanto Detalhes do Consumo da API

--- src/views/encontreAquiView.vue
+++ src/views/encontreAquiView.vue
@@ -6,69 +6,82 @@
       ENCONTRE UM PONTO DE VENDA MAIS PRÓXIMO DE VOCÊ
     </h2>
     <v-card v-for="item of localizacaoVendedores" :key="item.id" :loading="loading" class="mx-auto my-12" max-width="374">
-      <template slot="progress">
-        <v-progress-linear
-          color="deep-purple"
-          height="10"
-          indeterminate
-        ></v-progress-linear>
-      </template>
+    <template slot="progress">
+      <v-progress-linear
+        color="deep-purple"
+        height="10"
+        indeterminate
+      ></v-progress-linear>
+    </template>
 
-      <v-img
-        height="250"
-        :src="item.imagem"
-      ></v-img>
+    <v-img 
+      height="250"
+      src="item.imagem"
+    ></v-img>
 
-      <v-card-title>{{item.nome}}</v-card-title>
+    <v-card-title>{{item.nome}}</v-card-title>
 
-      <v-card-text>
-        <v-row align="center" class="mx-0">
-          <v-rating
-            :value="4.5"
-            color="amber"
-            dense
-            half-increments
-            readonly
-            size="14"
-          ></v-rating>
+    <v-card-text>
+      <v-row
+        align="center"
+        class="mx-0"
+      >
+        <v-rating
+          :value="4.5"
+          color="amber"
+          dense
+          half-increments
+          readonly
+          size="14"
+        ></v-rating>
 
-          <div class="grey--text ms-4">4.5 (413)</div>
-        </v-row>
-
-        <div class="my-4 text-subtitle-1">R$ {{item.preco}}</div>
-
-        <div>
-          Small plates, salads & sandwiches - an intimate setting with 12 indoor
-          seats plus patio seating.
+        <div class="grey--text ms-4">
+          4.5 (413)
         </div>
-      </v-card-text>
+      </v-row>
 
-      <v-divider class="mx-4"></v-divider>
+      <div class="my-4 text-subtitle-1">
+        R$ {{item.preco}}
+      </div>
 
-      <v-card-title>Tonight's availability</v-card-title>
+      <div class="font-weight-black text-center">{{item.local.nome}}</div>
 
-      <v-card-text>
-        <v-chip-group
-          v-model="selection"
-          active-class="deep-purple accent-4 white--text"
-          column
-        >
-          <v-chip>5:30PM</v-chip>
+      <div class="font-weight text-center">Endereço: {{item.local.endereco}}</div>
+      <div class="font-weight text-center">Cep: {{item.local.cep}}</div>
 
-          <v-chip>7:30PM</v-chip>
+  
+    </v-card-text>
 
-          <v-chip>8:00PM</v-chip>
+    <v-divider class="mx-4"></v-divider>
 
-          <v-chip>9:00PM</v-chip>
-        </v-chip-group>
-      </v-card-text>
+    <v-card-title>Tonight's availability</v-card-title>
 
-      <v-card-actions>
-        <v-btn color="deep-purple lighten-2" text @click="reserve">
-          Reserve
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+    <v-card-text>
+      <v-chip-group
+        v-model="selection"
+        active-class="deep-purple accent-4 white--text"
+        column
+      >
+        <v-chip>5:30PM</v-chip>
+
+        <v-chip>7:30PM</v-chip>
+
+        <v-chip>8:00PM</v-chip>
+
+        <v-chip>9:00PM</v-chip>
+      </v-chip-group>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn
+        color="deep-purple lighten-2"
+        text
+        @click="reserve"
+      >
+        Reserve
+      </v-btn>
+    </v-card-actions>
+  </v-card>
   </v-container>
 </template>
 <script>
